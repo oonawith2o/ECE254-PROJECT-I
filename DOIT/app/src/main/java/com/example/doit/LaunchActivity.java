c405f470a0a82fbc00a23a80d6a4e8de50cde14d
Oona Kintscher <oona.kintscher@gmail.com>
2023-10-02 17:45:28 -0500

    Test Project - Added Some Design Features

--- DOIT/app/src/main/java/com/example/doit/LaunchActivity.java
+++ DOIT/app/src/main/java/com/example/doit/LaunchActivity.java
@@ -2,7 +2,9 @@ package com.example.doit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LaunchActivity.this, MainActivity.class));
+                finish();
+            }
+        } , 3000);
     }
 }
